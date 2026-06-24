caae476f1f55e13aa179e7dea24b04eb311cf50c
tatvahome <team.tatva.living@gmail.com>
2026-06-24 22:06:48 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from '@tanstack/react-start/config'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  server: {
-    preset: 'vercel',
+  plugins: [react(), tsconfigPaths()],
+  build: {
+    outDir: 'dist',
   },
 })
